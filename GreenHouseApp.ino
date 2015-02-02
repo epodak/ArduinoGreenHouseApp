0243438550bd551e17ab5ea3449d7051c4de30fb
Vladimir Mitic <miticv@gmail.com>
2015-02-02 16:23:31 -0500

    Added website

--- GreenHouseApp.ino
+++ GreenHouseApp.ino
@@ -1,54 +1,41 @@
 
+//ethernet SD chip uses digital pins 10, 11, 12, and 13 for SPI buss.
+
+
 #include <SPI.h>
 #include <SD.h>
+#include <Ethernet.h>
 
 
 
 class SDCard
 {
 public:
-	void init(int chipSelected){
+
+	void initRoot(int chipSelected)
+	{
 		chipSelect = chipSelected;
-		// see if the card is present and can be initialized:
-			if (!SD.begin(chipSelect))
-			{
-				Serial.println("Card failed, or not present");
-				// don't do anything more:
-				return;
-			}
-		Serial.println("card initialized.");
-	};
+		if (!card.init(SPI_HALF_SPEED, chipSelect)){
+			Serial.println("initialization failed.");
+			return;
+		}
+
+		if (!volume.init(card)) {
+			Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+			return;
+		}
+
+		root.openRoot(volume);
+
+	}
 
 	void SDWriteLogPins(String DateTimeStamp)
 	{
+		initRoot(chipSelect);
 
-
-
-		/*
-		SD card datalogger
-		This example shows how to log data from three analog sensors
-		to an SD card using the SD library.
-		The circuit:
-		* analog sensors on analog ins 0, 1, and 2
-		* SD card attached to SPI bus as follows:
-		** MOSI - pin 11
-		** MISO - pin 12
-		** CLK - pin 13
-		** CS - pin 4
-
-		created  24 Nov 2010
-		modified 9 Apr 2012
-		by Tom Igoe
-
-		This example code is in the public domain.
-
-		*/
+		
 		int numberOfPins = 6; //number of analog pins to log
-		//Serial.print("Initializing SD card...");
-		// make a string for assembling the data to log:
 		String dataString = DateTimeStamp + ":";
-
-		// read three sensors and append to the string:
 		for (int analogPin = 0; analogPin < numberOfPins; analogPin++) {
 			int sensor = analogRead(analogPin);
 			dataString += String(sensor);
@@ -56,67 +43,27 @@ public:
 				dataString += ",";
 			}
 		}
-		// open the file. note that only one file can be open at a time,
-		// so you have to close this one before opening another.
-		File dataFile = SD.open("datalog.txt", FILE_WRITE);
 
-		// if the file is available, write to it:
-		if (dataFile) {
-			dataFile.println(dataString);
-			dataFile.close();
+		SdFile file;
+		file.open(root, "datalog.txt", O_CREAT | O_APPEND | O_WRITE);    //Open or create the file 'name' in 'root' for writing to the end of the file.
+		if (file.isFile()){
+
+			file.println(dataString);
+			file.close();
 			// print to the serial port too:
 			Serial.println(dataString);
 		}
-		else {
-			// if the file isn't open, pop up an error:
+		else{
 			Serial.println("error opening datalog.txt");
 		}
-
-
 	}
 
 	void SDGetCardInfo()
 	{
-		Sd2Card card;
-		SdVolume volume;
-		SdFile root;
 
 
-		// we'll use the initialization code from the utility libraries
-		// since we're just testing if the card is working!
-		if (!card.init(SPI_HALF_SPEED, chipSelect)){
-			Serial.println("initialization failed. Things to check:");
-			/*Serial.println("* is a card is inserted?");
-			Serial.println("* Is your wiring correct?");
-			Serial.println("* did you change the chipSelect pin to match your shield or module?");*/
-			return;
-		}
-		else {
-			Serial.println("Wiring is correct and a card is present.");
-		}
 
-		// print the type of card
-		Serial.print("\nCard type: ");
-		switch (card.type()) {
-		case SD_CARD_TYPE_SD1:
-			Serial.println("SD1");
-			break;
-		case SD_CARD_TYPE_SD2:
-			Serial.println("SD2");
-			break;
-		case SD_CARD_TYPE_SDHC:
-			Serial.println("SDHC");
-			break;
-		default:
-			Serial.println("Unknown");
-		}
-
-		// Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-		if (!volume.init(card)) {
-			Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-			return;
-		}
-
+		initRoot(chipSelect);
 
 		// print the type and size of the first FAT-type volume
 		uint32_t volumesize;
@@ -127,26 +74,33 @@ public:
 		volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
 		volumesize *= volume.clusterCount();       // we'll have a lot of clusters
 		volumesize *= 512;                            // SD card blocks are always 512 bytes
-		Serial.print("Volume size (bytes): ");
-		Serial.println(volumesize);
-		Serial.print("Volume size (Kbytes): ");
-		volumesize /= 1024;
-		Serial.println(volumesize);
 		Serial.print("Volume size (Mbytes): ");
 		volumesize /= 1024;
 		Serial.println(volumesize);
 
-
-		Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-		root.openRoot(volume);
-
 		// list all files in the card with date and size
 		root.ls(LS_R | LS_DATE | LS_SIZE);
-
+		Serial.println();
 	}
+
+	//boolean WebFileExists(){
+	//	if(!SD.exists("index.htm")) {
+	//		Serial.println("ERROR - Can't find index.htm file!");
+	//		return false;  // can't find index file
+	//	}
+	//	else
+	//	{
+	//		return true;
+	//	}
+	//	
+	//}
 
 private:
 	int chipSelect;
+	Sd2Card card;
+	SdVolume volume;
+	SdFile root;
+
 };
 
 
@@ -155,11 +109,20 @@ private:
 // 53 on the Mega) must be left as an output or the SD library
 // functions will not work.
 const int chipSelect = 4;
-const int outputEthernetShieldSDPin = 10; //to turn on SD library
+const int outputEthernetShieldSDPin = 10; //to turn on SD chip (disables ethernet)
+const int outputEthernetShieldEthernetPin = 4; //to turn on Ethernet chip (disables SD)
 SDCard *card;
+
+// MAC address from Ethernet shield sticker under board
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(10, 0, 0, 20); // IP address, may need to change depending on network
+EthernetServer server(80);  // create a server at port 80
+
+File webFile;
 
 void setup()
 {
+
 	// Open serial communications and wait for port to open:
 	Serial.begin(9600);
 	while (!Serial) {
@@ -167,13 +130,19 @@ void setup()
 	}
 
 	card = new SDCard();
-	card->init(chipSelect);
+	card->initRoot(chipSelect);
+	//card->WebFileExists();
+
+
+	Ethernet.begin(mac, ip);  // initialize Ethernet device
+	server.begin();           // start to listen for clients
 
 	// make sure that the default chip select pin is set to
 	// output, even if you don't use it:
-	pinMode(outputEthernetShieldSDPin, OUTPUT);
-	digitalWrite(outputEthernetShieldSDPin, HIGH);
-	
+	/*pinMode(outputEthernetShieldSDPin, OUTPUT);*/
+	//digitalWrite(outputEthernetShieldSDPin, HIGH);
+
+	//pinMode(outputEthernetShieldEthernetPin, OUTPUT);
 
 
 
@@ -181,6 +150,8 @@ void setup()
 
 void loop()
 {
+
+	checkWebRequest();
 
 	card->SDWriteLogPins("2015-01-12 12:00:01");
 	card->SDGetCardInfo();
@@ -190,4 +161,50 @@ void loop()
 }
 
 
+void checkWebRequest()
+{
 
+	EthernetClient client = server.available();  // try to get client
+	if (client) {  // got client?
+		boolean currentLineIsBlank = true;
+		while (client.connected()) {
+			if (client.available()) {   // client data available to read
+				char c = client.read(); // read 1 byte (character) from client
+				// last line of client request is blank and ends with \n
+				// respond to client only after last line received
+				if (c == '\n' && currentLineIsBlank) {
+					// send a standard http response header
+					client.println("HTTP/1.1 200 OK");
+					client.println("Content-Type: text/html");
+					client.println("Connection: close");
+					client.println();
+					// send web page
+					webFile = SD.open("index.htm");        // open web page file
+					if (webFile) {
+						while (webFile.available()) {
+							client.write(webFile.read()); // send web page to client
+						}
+						webFile.close();
+					}
+					break;
+				}
+				// every line of text received from the client ends with \r\n
+				if (c == '\n') {
+					// last character on line of received text
+					// starting new line with next character read
+					currentLineIsBlank = true;
+				}
+				else if (c != '\r') {
+					// a text character was received from client
+					currentLineIsBlank = false;
+				}
+			} // end if (client.available())
+		} // end while (client.connected())
+		delay(1000);      // give the web browser time to receive the data
+		client.stop(); // close the connection
+	}
+	else{
+		Serial.println(" - no clients.");
+	} // end if (client)
+
+}
