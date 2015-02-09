4b94294eb6115cffc2d33d124babec8ac27d2542
Vladimir Mitic <miticv@gmail.com>
2015-02-09 03:32:43 -0500

    WebServer Serve 2 pages

--- GreenHouseApp.ino
+++ GreenHouseApp.ino
@@ -8,6 +8,7 @@
 
 
 
+
 class SDCard
 {
 public:
@@ -18,12 +19,13 @@ public:
 	{
 		chipSelect = chipSelected;
 		if (!card.init(SPI_HALF_SPEED, chipSelect)){
-			Serial.println("initialization failed.");
+			Serialprintln("initialization failed.");
 			return;
 		}
 
 		if (!volume.init(card)) {
-			Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+ 
+			Serialprintln("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
 			return;
 		}
 
@@ -33,9 +35,9 @@ public:
 
 	void SDWriteLogPins(String DateTimeStamp)
 	{
-		initRoot(chipSelect);
+		root.openRoot(volume);
+		//initRoot(chipSelect);
 
-		
 		int numberOfPins = 6; //number of analog pins to log
 		String dataString = DateTimeStamp + ":";
 		for (int analogPin = 0; analogPin < numberOfPins; analogPin++) {
@@ -53,49 +55,46 @@ public:
 			file.println(dataString);
 			file.close();
 			// print to the serial port too:
-			Serial.println(dataString);
+			Serialprintln(dataString);
 		}
 		else{
-			Serial.println("error opening datalog.txt");
+			Serialprintln("error opening datalog.txt");
 		}
 	}
 
 	void SDGetCardInfo()
 	{
-
-
-
-		initRoot(chipSelect);
+		root.openRoot(volume);
+		//initRoot(chipSelect);
 
 		// print the type and size of the first FAT-type volume
 		uint32_t volumesize;
-		Serial.print("\nVolume type is FAT");
+		Serialprint("\nVolume type is FAT");
 		Serial.println(volume.fatType(), DEC);
-		Serial.println();
-
+		Serialprintln();
+		
 		volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
 		volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-		volumesize *= 512;                            // SD card blocks are always 512 bytes
-		Serial.print("Volume size (Mbytes): ");
+		volumesize *= 512;                            // SD card blocks are always 512 bytes 
+		Serialprint("Volume size (Mbytes): ");
 		volumesize /= 1024;
-		Serial.println(volumesize);
-
+		Serialprintln(volumesize);
 		// list all files in the card with date and size
 		root.ls(LS_R | LS_DATE | LS_SIZE);
-		Serial.println();
+		Serialprintln();
 	}
 
 	bool GetFile(char *filename){
 		file.open(root, filename, O_READ);
-		if(!file.isFile()) {
-			Serial.println("ERROR - Can't find file!");
+		if (!file.isFile()) {
+			Serialprintln("ERROR - Can't find file!");
 			return false;  // can't find index file
 		}
 		else
 		{
 			return true;
 		}
-		
+
 	}
 
 private:
@@ -107,13 +106,15 @@ private:
 };
 
 
-// On the Ethernet Shield, CS is pin 4. Note that even if it's not
-// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
-// 53 on the Mega) must be left as an output or the SD library
-// functions will not work.
-const int chipSelect = 4;
-const int outputEthernetShieldSDPin = 10; //to turn on SD chip (disables ethernet)
-const int outputEthernetShieldEthernetPin = 4; //to turn on Ethernet chip (disables SD)
+
+// How big our line buffer should be. 100 is plenty!
+#define BUFSIZ 100
+#define TIMEOUTMS 2000
+
+#define outputEthernetPin 10
+#define outputSdPin 4
+#define outputLcdPin 8
+
 SDCard *card;
 
 // MAC address from Ethernet shield sticker under board
@@ -121,19 +122,20 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 2, 120); // IP address, may need to change depending on network
 EthernetServer server(80);  // create a server at port 80
 
-File webFile;
+//File webFile;
 
 void setup()
 {
 
 	// Open serial communications and wait for port to open:
-	Serial.begin(9600);
-	while (!Serial) {
-		; // wait for serial port to connect. Needed for Leonardo only
-	}
+	Serialbegin(9600);
+	
+	// disable w5100 SPI while starting SD
+	pinMode(outputEthernetPin, OUTPUT);
+	digitalWrite(outputEthernetPin, HIGH);
 
 	card = new SDCard();
-	card->initRoot(chipSelect);
+	card->initRoot(outputSdPin);
 	//card->WebFileExists();
 
 
@@ -167,68 +169,167 @@ void loop()
 void checkWebRequest()
 {
 	char *IndexFile = "index.htm";
+	char *DataFile = "datalog.txt";
+	char clientline[BUFSIZ];
+	int index = 0;
+	unsigned long timeoutStart = 0;
+
+
 
 	EthernetClient client = server.available();  // try to get client
 	if (client) {  // got client?
-		Serial.println("Client!");
-		boolean currentLineIsBlank = true;
+		Serialprintln("Client!");
+		timeoutStart = millis();
 		while (client.connected()) {
-			//Serial.println("Client Connected!");
+			//Serialprintln("Client Connected!");
 			if (client.available()) {   // client data available to read
-				//Serial.println("Client Available!");
+
+				if (millis() - timeoutStart > TIMEOUTMS){
+					client.println("HTTP/1.1 404 Not Found");
+					client.println("Content-Type: text/html");
+					client.println("Connection: close");
+					client.println();
+					client.println("<html><head></head><body><h2>Timeout reached</h2><body></html>");
+					//delay(1);
+					//client.stop(); // close the connection
+					break;
+				}
+
+
 				char c = client.read(); // read 1 byte (character) from client
-				// last line of client request is blank and ends with \n
-				// respond to client only after last line received
-				Serial.print(c); 
-				if (c == '\n' && currentLineIsBlank) {
-					// send a standard http response header
-					Serial.println("ended :)"); 
+				// If it isn't a new line, add the character to the buffer
+				Serialprint(c);
+				if (c != '\n' && c != '\r') {
+					clientline[index] = c;
+					index++;
+					// are we too big for the buffer? start tossing out data
+					if (index >= BUFSIZ)
+						index = BUFSIZ - 1;
+
+					// continue to read more data!					
+					continue; //go back to begining of while loop!
+				}
+				// got a \n or \r new line, which means the string is done
+				/*Examples:
+				GET / HTTP/1.1
+				GET /himidity HTTP/1.1
+				GET /temperature HTTP/1.1
+				GET /log HTTP/1.1
+				*/
+				clientline[index] = 0; //terminate string
+				if (strstr(clientline, "GET / ") != 0) {  //Returns Index file!
 					client.println("HTTP/1.1 200 OK");
 					client.println("Content-Type: text/html");
 					client.println("Connection: close");
 					client.println();
+
 					// send web page   
 					if (card->GetFile(IndexFile))
-					{						
+					{
 						int16_t inchar = card->file.read();
-						Serial.println(inchar);
-						Serial.println("Got File!!");
-						while (inchar >= 0) {							
+						Serialprintln(inchar);
+						Serialprintln("Got File!!");
+						while (inchar >= 0) {
 							client.print((char)inchar); // send web page to client
-							Serial.print((char)inchar);
 							inchar = card->file.read();
 						}
 						card->file.close();
+						
 					}
-					//webFile = SD.open("index.htm");        // open web page file
-					//if (webFile) {
-					//	while (webFile.available()) {
-					//		client.write(webFile.read()); // send web page to client
-					//	}
-					//	webFile.close();
-					//}
 					else{
-						Serial.println("Can not read Index!");
+						Serialprintln("Can not read Index!");
 					}
 					break;
 				}
-				// every line of text received from the client ends with \r\n
-				if (c == '\n') {
-					// last character on line of received text
-					// starting new line with next character read
-					currentLineIsBlank = true;
+				if (strstr(clientline, "GET /logfile ") != 0) {  //Returns size of log file file!
+					client.println("HTTP/1.1 200 OK");
+					client.println("Content-Type: application/json");
+					client.println("Connection: close");		
+					client.println();
+					client.println(5);
+					/*if (card->GetFile(DataFile))
+					{
+						card->file.open(
+						client.print("{size:");
+						client.print(String(card->file..fileSize(), DEC));
+						client.print("b}");
+						client.println();
+						card->file.close();
+					}
+					else{
+						Serialprintln("Can not read Data!");
+					}*/
+					break;
 				}
-				else if (c != '\r') {
-					// a text character was received from client
-					currentLineIsBlank = false;
+				else{
+					// everything else is a 404
+					client.println("HTTP/1.1 404 Not Found");
+					client.println("Content-Type: text/html");
+					client.println("Connection: close");
+					client.println();
+					client.println();
+					client.println("<html><head></head><body><h2>File Not Found!</h2><body></html>");
+					//delay(1);
+					//client.stop(); // close the connection
+					break;
 				}
-			} // end if (client.available())
-		} // end while (client.connected())
-		delay(5000);      // give the web browser time to receive the data
-		client.stop(); // close the connection
-	}
-	else{
-		Serial.println(" - no clients.");
-	} // end if (client)
+			}
+		} // end if (client.available())
+	} // end while (client.connected())
+	delay(1);      // give the web browser time to receive the data
+	client.stop(); // close the connection
+}
 
+
+/* Temp functions */
+// comment out the next line to eliminate the Serial.print stuff
+// saves about 1.6K of program memory
+#define ServerDEBUG 1
+void Serialbegin(unsigned long val){
+#ifdef ServerDEBUG 
+	Serial.begin(val);
+	while (!Serial) {
+		; // wait for serial port to connect. Needed for Leonardo only
+	}
+#endif
+}
+void Serialprint(char* val){ 
+#ifdef ServerDEBUG 
+	Serial.print(val);
+#endif
+}
+void Serialprint(char val){
+#ifdef ServerDEBUG 
+	Serial.print(val);
+#endif
+}
+void Serialprint(const String &val){
+#ifdef ServerDEBUG 
+	Serial.print(val);
+#endif
+}
+void Serialprint(unsigned int val1, int val2){
+#ifdef ServerDEBUG 
+	Serial.print(val1, val2);
+#endif
+}
+void Serialprintln(char* val){
+#ifdef ServerDEBUG 
+	Serial.println(val);
+#endif
+}
+void Serialprintln(uint32_t val){
+#ifdef ServerDEBUG 
+	Serial.println(val);
+#endif
+}
+void Serialprintln(){
+#ifdef ServerDEBUG 
+	Serial.println();
+#endif
+}
+void Serialprintln(const String &val){
+#ifdef ServerDEBUG 
+	Serial.print(val);
+#endif
 }
